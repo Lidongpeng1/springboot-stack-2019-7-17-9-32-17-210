a544e0b85f93801336e6656fde89690b4b1f899f
“your_uLiDongpeng1 <l>
2019-07-17 23:31:54 +0800

    case story AC2 is finish

---
+++ src/main/java/com/tw/apistackbase/respository/CaseResposity.java
@@ -0,0 +1,12 @@
+package com.tw.apistackbase.respository;
+
+import com.tw.apistackbase.entiey.Case;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CaseResposity extends JpaRepository<Case,Integer> {
+
+     Case findAllCaseById(int id);
+
+}
