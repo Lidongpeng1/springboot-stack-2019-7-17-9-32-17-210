040951fe8bc05d14aa8d421b3160d0dbc0ec05bd
“your_uLiDongpeng1 <l>
2019-07-17 23:58:36 +0800

    story3.1 AC2 is finish

---
+++ src/main/java/com/tw/apistackbase/service/ProcuratorateService.java
@@ -0,0 +1,15 @@
+package com.tw.apistackbase.service;
+
+import com.tw.apistackbase.model.Procuratorate;
+import com.tw.apistackbase.repository.ProcuratorateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProcuratorateService {
+    @Autowired
+    private ProcuratorateRepository repository;
+
+}
